745c4d7c96149fb284b1248667a0f5c22cbd6491
Vinnicius Gomes <vinniciusgomes@outlook.com.br>
2021-10-21 10:20:05 -0300

    Atualizando arquitetura

--- react-architecture/src/services/api.ts
+++ react-architecture/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { env } from 'config';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: env.apiUrl,
 });
 
 export default api;
